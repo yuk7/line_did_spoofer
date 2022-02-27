b39333a190dec4d26f9244bd67d7124e66cf8561
yuk7 <yukx00@gmail.com>
2022-02-28 08:59:39 +0900

    hooks: Wrote AndroidIDHook

--- app/src/main/java/io/github/yuk7/idspooflinex/hooks/AndroidIDHook.kt
+++ app/src/main/java/io/github/yuk7/idspooflinex/hooks/AndroidIDHook.kt
@@ -1,6 +1,7 @@
 package io.github.yuk7.idspooflinex.hooks
 
 import android.app.Application
+import android.content.ContentResolver
 import android.content.Context
 import android.net.Uri
 import android.provider.Settings
@@ -12,6 +13,7 @@ import de.robv.android.xposed.XposedHelpers
 import de.robv.android.xposed.callbacks.XC_LoadPackage
 import io.github.yuk7.idspooflinex.preferences.PreferenceKeys
 import io.github.yuk7.idspooflinex.providers.SettingProvider
+import io.github.yuk7.idspooflinex.utils.AndroidIDUtils
 
 class AndroidIDHook : IXposedHookLoadPackage {
     override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam?) {
@@ -46,10 +48,33 @@ class AndroidIDHook : IXposedHookLoadPackage {
                     }
                     cursorPreferences.close()
 
-                    XposedBridge.log("LINE MOD ANDROID_ID: ${preferencesMap[PreferenceKeys.keyAndroidId]}")
+                    val newAndroidID = preferencesMap[PreferenceKeys.keyAndroidId]
+
+                    XposedBridge.log("LINE MOD ANDROID_ID: $newAndroidID")
+                    if (!AndroidIDUtils.isValidAndroidID(newAndroidID)) {
+                        XposedBridge.log("ERROR: LINE MOD ANDROID_ID is invalid.")
+                        return
+                    }
+
+
+                    XposedHelpers.findAndHookMethod(
+                        Settings.Secure::class.java.name,
+                        lpparam.classLoader,
+                        "getString",
+                        ContentResolver::class.java,
+                        String::class.java,
+                        object : XC_MethodHook() {
+                            override fun beforeHookedMethod(param: MethodHookParam?) {
+                                super.beforeHookedMethod(param)
+                                if (param!!.args[1].equals(Settings.Secure.ANDROID_ID)) {
+                                    param.result = newAndroidID
+                                }
+                            }
+                        }
+                    )
                 }
             }
         )
-
     }
+
 }
