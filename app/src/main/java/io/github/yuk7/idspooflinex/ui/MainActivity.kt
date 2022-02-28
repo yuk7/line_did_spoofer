d06878b1088ec748dc289498f6e9ccfdee388f48
yuk7 <yukx00@gmail.com>
2022-02-28 09:41:33 +0900

    ui: MainActivity: add hide switch

--- app/src/main/java/io/github/yuk7/idspooflinex/ui/MainActivity.kt
+++ app/src/main/java/io/github/yuk7/idspooflinex/ui/MainActivity.kt
@@ -1,8 +1,13 @@
 package io.github.yuk7.idspooflinex.ui
 
+import android.content.ComponentName
+import android.content.pm.PackageManager
+import android.content.pm.PackageManager.COMPONENT_ENABLED_STATE_DISABLED
+import android.content.pm.PackageManager.COMPONENT_ENABLED_STATE_ENABLED
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Switch
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.PreferenceManager
@@ -19,8 +24,13 @@ class MainActivity : AppCompatActivity() {
         val pref = PreferenceManager.getDefaultSharedPreferences(this)
 
         val btnSave = findViewById<Button>(R.id.buttonSave)
+        val switchVisible = findViewById<Switch>(R.id.switchVisibleInLauncher)
         val editTextAndroidID = findViewById<EditText>(R.id.editTextAndroidID)
+
         editTextAndroidID.setText(pref.getString(PreferenceKeys.keyAndroidId, ""))
+        val ces = packageManager.getComponentEnabledSetting(componentName)
+        switchVisible.isChecked = ces != PackageManager.COMPONENT_ENABLED_STATE_DISABLED
+
         btnSave.setOnClickListener {
             editTextAndroidID.setText(editTextAndroidID.text.toString().lowercase())
             val newAndroidID = editTextAndroidID.text.toString()
@@ -36,7 +46,29 @@ class MainActivity : AppCompatActivity() {
             }
             pref.edit().putString(PreferenceKeys.keyAndroidId, newAndroidID).apply()
         }
+
+        switchVisible.setOnCheckedChangeListener { _, isChecked  ->
+            if (isChecked) {
+                packageManager.setComponentEnabledSetting(
+                    componentName,
+                    COMPONENT_ENABLED_STATE_ENABLED,
+                    PackageManager.DONT_KILL_APP
+                )
+            } else {
+                AlertDialog.Builder(this)
+                    .setTitle("Caution")
+                    .setMessage(("Press \"Yes\" to hide the app from the launcher.\n" +
+                            "You may need to reinstall the application to reconfigure it."))
+                    .setPositiveButton(
+                        "Yes"
+                    ) { _, _ -> packageManager.setComponentEnabledSetting(
+                        componentName,
+                        COMPONENT_ENABLED_STATE_DISABLED,
+                        PackageManager.DONT_KILL_APP)
+                    }
+                    .setNegativeButton("No") {_, _ ->}
+                    .show()
+            }
+        }
     }
-
-
 }
